5a737e108d1ebffda17500c73c7145d63734d0d0
Alex <Alex@cepegra.be>
2026-03-25 08:16:44 +0100

    corrige header,footer,home

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // 1. Находим элементы (проверь, что классы и ID совпадают с HTML)
+  
   const btnOpen = document.querySelector('.btn--secondary'); // Твоя кнопка "BROWSE BY CATEGORY"
   const popup = document.querySelector('#popup-category');   // Новый ID попапа
   const btnClose = document.querySelector('#close-popup-cat'); // Крестик
   const btnCancel = document.querySelector('#btn-cancel');     // Кнопка CANCEL
 
-  // 2. Проверяем, что кнопка и попап существуют, чтобы не было ошибок в консоли
+  
   if (btnOpen && popup) {
       
       // ОТКРЫТИЕ ПОПАПА
       btnOpen.addEventListener('click', (e) => {
-          e.preventDefault(); // Чтобы страница не дергалась вверх
-          popup.classList.add('popup--active'); // Добавляем класс видимости
-          document.body.style.overflow = 'hidden'; // Запрещаем скролл сайта под окном
+          e.preventDefault(); 
+          popup.classList.add('popup--active'); 
+          document.body.style.overflow = 'hidden'; 
       });
 
       // ФУНКЦИЯ ЗАКРЫТИЯ
       const closePopup = () => {
-          popup.classList.remove('popup--active'); // Убираем класс видимости
-          document.body.style.overflow = 'auto'; // Возвращаем скролл сайта
+          popup.classList.remove('popup--active'); 
+          document.body.style.overflow = 'auto'; 
       };
 
       // ЗАКРЫТИЕ (по клику на крестик или на кнопку CANCEL)
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (overlay) overlay.addEventListener('click', closePopup);
 
   } else {
-      // Если что-то не нашлось, скрипт напишет об этом в консоль (F12)
+      
       console.error("Ошибка: Проверь, что у кнопки есть класс .btn--secondary, а у попапа ID #popup-category");
   }
 });
